b0c3cc195780b875a1b857613586e61b822e2e3c
Justin Conner <jstncnnr@outlook.com>
2025-04-17 22:06:19 -0700

    feat: manage config for zsh, ghostty, and oh-my-posh

--- home/justin/default.nix
+++ home/justin/default.nix
@@ -1,4 +1,4 @@
-{ pkgs, ... }:
+{ lib, pkgs, ... }:
 
 {
   home.packages = [
@@ -17,5 +17,140 @@
     extraConfig = ''
       IdentityAgent ~/.1password/agent.sock
     '';
+  };
+
+  programs.ghostty = {
+    enable = true;
+    enableZshIntegration = true;
+
+    settings = {
+      font-family = "MesloLGS Nerd Font Mono";
+      font-size = 12;
+      background-opacity = 0.8;
+      background-blur = 8;
+      window-theme = "system";
+      macos-titlebar-style = "native";
+    };
+  };
+
+  programs.zsh = {
+    enable = true;
+
+    enableCompletion = true;
+    autosuggestion.enable = true;
+    syntaxHighlighting.enable = true;
+  };
+
+  programs.oh-my-posh = {
+    enable = true;
+    enableZshIntegration = true;
+
+    settings = {
+      version = 3;
+      final_space = true;
+      console_title_template = "{{ .Shell }} in {{ .Folder }}";
+      palette = {
+        black = "#262b44";
+	blue = "#4b95e9";
+	green = "#59c9a5";
+	orange = "#f07623";
+	red = "#d81e5b";
+	white = "#e0def4";
+	yellow = "#f3ae35";
+      };
+      transient_prompt = {
+        template = " ";
+	foreground = "p:white";
+	background = "transparent";
+      };
+      blocks = [
+        {
+          type = "prompt";
+	  alignment = "left";
+	  newline = true;
+	  segments = [
+            {
+	      type = "os";
+	      style = "powerline";
+	      template = " {{ .Icon }} ";
+	      foreground = "p:black";
+	      background = "p:white";
+	      powerline_symbol = "";
+	      leading_powerline_symbol = "<transparent>";
+	    }
+	    {
+	      type = "session";
+	      style = "powerline";
+	      template = "{{ if .SSHSession }} {{ .UserName }}@{{ .HostName }} {{ end }}";
+	      foreground = "p:black";
+	      background = "p:red";
+	      powerline_symbol = "";
+	    }
+	    {
+	      type = "path";
+	      style = "powerline";
+	      template = "  {{ .Path }} ";
+	      foreground = "p:white";
+	      background = "p:blue";
+	      powerline_symbol = "";
+	      properties.style = "powerlevel";
+	    }
+	    {
+	      type = "git";
+	      style = "powerline";
+	      templates = [
+                " {{ .HEAD }}"
+		"{{ if .BranchStatus }} {{ .BranchStatus }} {{ end }}"
+		"{{ if .Working.Changed }}  {{ .Working.String }}{{ end }}"
+		"{{ if .Staging.Changed }}  {{ .Staging.String }}{{ end }}"
+	      ];
+	      templates_logic = "join";
+	      foreground = "p:black";
+	      background = "p:green";
+	      foreground_templates = [
+                "{{ if or (.Working.Changed) (.Staging.Changed) }}p:black{{ end }}"
+		"{{ if and (gt .Ahead 0) (gt .Behind 0) }}p:white{{ end }}"
+		"{{ if gt .Ahead 0 }}p:white{{ end }}"
+	      ];
+	      background_templates = [
+                "{{ if or (.Working.Changed) (.Staging.Changed) }}p:yellow{{ end }}"
+		"{{ if and (gt .Ahead 0) (gt .Behind 0) }}p:red{{ end }}"
+		"{{ if gt .Ahead 0 }}#49416d{{ end }}"
+		"{{ if gt .Behind 0 }}#7a306c{{ end }}"
+	      ];
+	      powerline_symbol = "";
+	    }
+	  ];
+	}
+	{
+	  type = "prompt";
+	  alignment = "right";
+	  segments = [
+            {
+	      type = "executiontime";
+	      style = "powerline";
+	      foreground = "p:black";
+	      background = "p:yellow";
+	      powerline_symbol = "<transparent>";
+	      leading_powerline_symbol = "";
+	      properties.style = "round";
+	    }
+	  ];
+	}
+	{
+	  type = "prompt";
+	  alignment = "left";
+	  newline = true;
+	  segments = [
+            {
+	      type = "text";
+	      style = "plain";
+	      template = "";
+	      foreground = "p:white";
+	    }
+	  ];
+	}
+      ];
+    };
   };
 }
