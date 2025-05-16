2d55ab25a2a4df4448b6d42fac8d551c05a0c7de
Justin Conner <jstncnnr@outlook.com>
2025-05-16 10:10:49 -0700

    feat(nixos): add some gnome extensions

--- modules/nixos/desktopEnvironment/default.nix
+++ modules/nixos/desktopEnvironment/default.nix
@@ -1,10 +1,14 @@
-{ lib, config, pkgs, ... }:
-let
+{
+  lib,
+  config,
+  pkgs,
+  ...
+}: let
   cfg = config.system;
 in {
   options = {
     # We only allow one desktop environment to be active at a time.
-    # This is mostly due to the tight integration with Gnome/GDM. 
+    # This is mostly due to the tight integration with Gnome/GDM.
     system.desktopEnvironment = lib.mkOption {
       # TODO: Add support for plasma, xfce, cinnamon, etc.
       type = lib.types.nullOr (lib.types.enum ["gnome"]);
@@ -13,12 +17,22 @@ in {
     };
   };
 
-  config = lib.mkIf (cfg.desktopEnvironment != null) {
-    # Is there any global config we can set no matter the DE?
-  } // lib.mkIf (cfg.desktopEnvironment == "gnome") {
-    services.xserver.desktopManager.gnome.enable = true;
-    services.udev.packages = [
-      pkgs.gnome-settings-daemon
-    ];
-  };
+  config =
+    lib.mkIf (cfg.desktopEnvironment != null) {
+      # Is there any global config we can set no matter the DE?
+    }
+    // lib.mkIf (cfg.desktopEnvironment == "gnome") {
+      services.xserver.desktopManager.gnome.enable = true;
+
+      services.udev.packages = [
+        pkgs.gnome-settings-daemon
+      ];
+
+      environment.systemPackages = [
+        pkgs.gnomeExtensions.blur-my-shell
+        pkgs.gnomeExtensions.just-perfection
+        pkgs.gnomeExtensions.no-overview
+        pkgs.gnomeExtensions.space-bar
+      ];
+    };
 }
