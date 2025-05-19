f2e9a52dacd920931be91e4f7746736cd59b73a2
Justin Conner <jstncnnr@outlook.com>
2025-05-19 09:54:24 -0700

    fix: typo in devenv cachix public key

--- modules/nixos/core/default.nix
+++ modules/nixos/core/default.nix
@@ -20,7 +20,7 @@
 
     extraOptions = ''
       extra-substituters = https://devenv.cachix.org
-      extra-trusted-public-keys = devenv.cachix.org-1:w1cLUi8dv3hnoSPGAuiqBv+f9TZLr6cv/Hm9XgU50cw=
+      extra-trusted-public-keys = devenv.cachix.org-1:w1cLUi8dv3hnoSPGAuibQv+f9TZLr6cv/Hm9XgU50cw=
     '';
   };
 
